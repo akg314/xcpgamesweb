60b902c22e08c05be50c3e8f7b616ddaaaf8607c
hikergirl1234 <alexandrakgore@gmail.com>
2017-11-02 20:49:02 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const dom = new JSDOM('<!doctype html><html><body></body></html>', { url: 'http://aquabox:8080' });
+global.window = dom.window;
+global.document = window.document;
+global.navigator = window.navigator;
 require("./project").server({
   httpPort: process.env.PORT || 8080 // Optional, but added here for demo purposes
 });
