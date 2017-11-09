0d22576372b6407298c0fbe44b15e9f4c217cbec
hikergirl1234 <alexandrakgore@gmail.com>
2017-11-08 19:43:41 -0800

    trade panel edits

---
+++ src/components/client.js
@@ -0,0 +1,3 @@
+require('./components/card-binder/index.marko')
+    .renderSync({})
+    .appendTo(document.body);
---
+++ src/components/util/helperfunctions.js
@@ -0,0 +1,22 @@
+var moment = require('moment');
+//utility functions for displaying trade data
+exports.capitalize = function(string) {
+    return string[0].toUpperCase()+string.slice(1);
+}
+exports.round = function(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+};
+exports.abbreviateAsset = function(string) {
+    if(string == "BITCRYSTALS") {
+        return "bcy"
+    } else { 
+        return string
+    }
+}
+exports.formatDateMD = function(date) {
+    return moment(date, "YYYY-MM-DDTHH:mm:ssZ").format('M-DD');
+}
+exports.formatDateMDY = function(date) {
+    return moment(date, "YYYY-MM-DDTHH:mm:ssZ").format('M-DD-YY');
+}
+
